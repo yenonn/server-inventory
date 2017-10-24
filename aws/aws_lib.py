30638dfe84106db5ced17fa408f0038f18886d88
Yenonn Hiu <yenonn.hiu@lynxanalytics.com>
2017-10-24 21:07:00 +0800

    Convert tabs into spaces

--- aws/aws_lib.py
+++ aws/aws_lib.py
@@ -9,7 +9,7 @@ service_client_list = [
 
 # get the size in human readable format
 def sizeof_fmt(num):
-  for x in ['bytes', 'KB', 'MB', 'GB', 'TB' ]:
+  for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
     if num < 1024.0:
       return "%3.1f %s" % (num, x)
     num /= 1024.0
