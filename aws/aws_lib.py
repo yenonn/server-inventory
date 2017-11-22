fdffd63826280cf31276174138efb44851b6d0d0
hiuy <yenonn.hiu@lynxanalytics.com>
2017-11-22 10:47:17 +0800

    Fixing 2 space indentation

--- aws/aws_lib.py
+++ aws/aws_lib.py
@@ -12,143 +12,143 @@ service_client_list = [
 
 
 def sizeof_fmt(num):
-    for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
-        if num < 1024.0:
-            return "%3.1f %s" % (num, x)
-        num /= 1024.0
+  for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+    if num < 1024.0:
+      return "%3.1f %s" % (num, x)
+    num /= 1024.0
 
 
 def get_client(service_client):
-    if service_client in service_client_list:
-        return boto3.client(service_client)
-    else:
-        return None
+  if service_client in service_client_list:
+    return boto3.client(service_client)
+  else:
+    return None
 
 
 def get_resources(service_client, region):
-    if service_client in service_client_list:
-        return boto3.resource(service_client, region_name=region)
-    else:
-        return None
+  if service_client in service_client_list:
+    return boto3.resource(service_client, region_name=region)
+  else:
+    return None
 
 
 def get_regions():
-    regions = get_client('ec2').describe_regions()["Regions"]
-    return [region['RegionName'] for region in regions]
+  regions = get_client('ec2').describe_regions()["Regions"]
+  return [region['RegionName'] for region in regions]
 
 
 def get_ec2_instances(region):
-    ec2_instances = get_resources('ec2', region)
-    return ec2_instances.instances.filter(
-        Filters=[{
-            'Name': 'instance-state-name',
-            'Values': ['*']
-        }]
-    )
+  ec2_instances = get_resources('ec2', region)
+  return ec2_instances.instances.filter(
+      Filters=[{
+          'Name': 'instance-state-name',
+          'Values': ['*']
+      }]
+  )
 
 
 def get_ec2_ebs(region):
-    ec2_instances = get_resources('ec2', region)
-    return ec2_instances.volumes.all()
+  ec2_instances = get_resources('ec2', region)
+  return ec2_instances.volumes.all()
 
 
 def show_ebs_volumes():
-    for region in get_regions():
-        print("###(Region:{})###".format(region))
-        for vol in get_ec2_ebs(region):
-            print(
-                "ID: {}({}): {}GB, Created: {}".format(
-                    vol.id,
-                    vol.state,
-                    vol.size,
-                    vol.create_time))
+  for region in get_regions():
+    print("###(Region:{})###".format(region))
+    for vol in get_ec2_ebs(region):
+      print(
+          "ID: {}({}): {}GB, Created: {}".format(
+              vol.id,
+              vol.state,
+              vol.size,
+              vol.create_time))
 
 
 def show_ec2_instances():
-    for region in get_regions():
-        print("###(Region:{})###".format(region))
-        for instance in get_ec2_instances(region):
-            tags = {t['Key'].lower(): t['Value']
-                    for t in instance.tags} if instance.tags else {}
-            instance_state = instance.state['Name']
-            instance_name = tags.get('name', 'undefined')
-            print("  * {}: {}".format(instance_name, instance_state))
+  for region in get_regions():
+    print("###(Region:{})###".format(region))
+    for instance in get_ec2_instances(region):
+      tags = {t['Key'].lower(): t['Value']
+              for t in instance.tags} if instance.tags else {}
+      instance_state = instance.state['Name']
+      instance_name = tags.get('name', 'undefined')
+      print("  * {}: {}".format(instance_name, instance_state))
 
 
 def get_s3_buckets(region):
-    s3_instances = get_resources('s3', region)
-    return s3_instances.buckets.all()
+  s3_instances = get_resources('s3', region)
+  return s3_instances.buckets.all()
 
 
 def show_s3_buckets():
-    for region in get_regions():
-        print("###Region:({})###".format(region))
-        total_bucket_size = 0
-        for bucket in get_s3_buckets(region):
-            print(" * {}(Creation date: {})".format(bucket.name, bucket.creation_date))
-            for object in bucket.objects.all():
-                bucket_obj = get_resources(
-                    's3', region).Object(
-                    bucket.name, object.key)
-                total_bucket_size += int(bucket_obj.content_length)
-                print("   - {}(Last Modified: {} Size: {})".format(object.key,
-                                                                   object.last_modified, sizeof_fmt(bucket_obj.content_length)))
-            print(" Total size: {}".format(sizeof_fmt(total_bucket_size)))
+  for region in get_regions():
+    print("###Region:({})###".format(region))
+    total_bucket_size = 0
+    for bucket in get_s3_buckets(region):
+      print(" * {}(Creation date: {})".format(bucket.name, bucket.creation_date))
+      for object in bucket.objects.all():
+        bucket_obj = get_resources(
+            's3', region).Object(
+            bucket.name, object.key)
+        total_bucket_size += int(bucket_obj.content_length)
+        print("   - {}(Last Modified: {} Size: {})".format(object.key,
+                                                           object.last_modified, sizeof_fmt(bucket_obj.content_length)))
+      print(" Total size: {}".format(sizeof_fmt(total_bucket_size)))
 
 
 def show_s3_buckets_acl():
-    for region in get_regions():
-        print("###Region:({})###".format(region))
-        for bucket in get_s3_buckets(region):
-            print(" * {}".format(bucket.name))
-            for grant in bucket.Acl().grants:
-                grantee = grant['Grantee']
-                display_name = grantee.get("DisplayName", 'undefined')
-                uri = grantee.get("URI", 'undefined')
-                permission = grant['Permission']
-                if display_name == 'undefined':
-                    print("   - {}({})".format(uri, permission))
-                else:
-                    print("   - {}({})".format(display_name, permission))
+  for region in get_regions():
+    print("###Region:({})###".format(region))
+    for bucket in get_s3_buckets(region):
+      print(" * {}".format(bucket.name))
+      for grant in bucket.Acl().grants:
+        grantee = grant['Grantee']
+        display_name = grantee.get("DisplayName", 'undefined')
+        uri = grantee.get("URI", 'undefined')
+        permission = grant['Permission']
+        if display_name == 'undefined':
+          print("   - {}({})".format(uri, permission))
+        else:
+          print("   - {}({})".format(display_name, permission))
 
 
 def get_iam_users():
-    users = get_client('iam')
-    return users.list_users()['Users']
+  users = get_client('iam')
+  return users.list_users()['Users']
 
 
 def get_iam_groups(username):
-    groups = get_client('iam').list_groups_for_user(UserName=username)
-    return groups['Groups']
+  groups = get_client('iam').list_groups_for_user(UserName=username)
+  return groups['Groups']
 
 
 def get_iam_role_policies(rolename):
-    role_policies = get_client('iam').list_role_policies(RoleName=rolename)
-    return role_policies['PolicyNames']
+  role_policies = get_client('iam').list_role_policies(RoleName=rolename)
+  return role_policies['PolicyNames']
 
 
 def get_iam_roles():
-    roles = get_client('iam').list_roles()
-    return roles['Roles']
+  roles = get_client('iam').list_roles()
+  return roles['Roles']
 
 
 def show_rds_cluster():
-    for region in get_regions():
-        print("###Region:({})###".format(region))
-        rds_client = boto3.client('rds', region_name=region)
-        if len(rds_client.describe_db_clusters()['DBClusters']):
-            print(rds_client.describe_db_clusters()['DBClusters'])
+  for region in get_regions():
+    print("###Region:({})###".format(region))
+    rds_client = boto3.client('rds', region_name=region)
+    if len(rds_client.describe_db_clusters()['DBClusters']):
+      print(rds_client.describe_db_clusters()['DBClusters'])
 
 
 def show_rds_snapshots():
-    for region in get_regions():
-        print("###Region:({})###".format(region))
-        rds_client = boto3.client('rds', region_name=region)
-        if len(rds_client.describe_db_snapshots()['DBSnapshots']):
-            db_snapshot = rds_client.describe_db_snapshots()['DBSnapshots'][0]
-            for key, value in db_snapshot.items():
-                print("  {}: {}".format(key, value))
+  for region in get_regions():
+    print("###Region:({})###".format(region))
+    rds_client = boto3.client('rds', region_name=region)
+    if len(rds_client.describe_db_snapshots()['DBSnapshots']):
+      db_snapshot = rds_client.describe_db_snapshots()['DBSnapshots'][0]
+      for key, value in db_snapshot.items():
+        print("  {}: {}".format(key, value))
 
 
 if __name__ == '__main__':
-    show_ebs_volumes()
+  show_ebs_volumes()
--- aws/security_groups_scanner.py
+++ aws/security_groups_scanner.py
@@ -3,74 +3,74 @@ import boto3
 
 
 def scan_ec2_security_groups():
-    membership = {}
-    hazard_sg = {}
-    regions = boto3.client('ec2').describe_regions()['Regions']
-    for region in regions:
-        region_name = region['RegionName']
-        print("* Region Name: {}".format(region_name))
+  membership = {}
+  hazard_sg = {}
+  regions = boto3.client('ec2').describe_regions()['Regions']
+  for region in regions:
+    region_name = region['RegionName']
+    print("* Region Name: {}".format(region_name))
 
-        ec2 = boto3.resource('ec2', region_name=region['RegionName'])
-        running_instances = ec2.instances.filter(Filters=[{
-            'Name': 'instance-state-name',
-            'Values': ['running', 'stopped']}])
-        for instance in running_instances:
-            tags = {t['Key'].lower(): t['Value']
-                    for t in instance.tags} if instance.tags else {}
-            instance_state = instance.state['Name']
-            instance_name = tags.get('name', 'undefined')
-            instance_key_name = instance.key_name
+    ec2 = boto3.resource('ec2', region_name=region['RegionName'])
+    running_instances = ec2.instances.filter(Filters=[{
+        'Name': 'instance-state-name',
+        'Values': ['running', 'stopped']}])
+    for instance in running_instances:
+      tags = {t['Key'].lower(): t['Value']
+              for t in instance.tags} if instance.tags else {}
+      instance_state = instance.state['Name']
+      instance_name = tags.get('name', 'undefined')
+      instance_key_name = instance.key_name
 
-            for sg in instance.security_groups:
-                security_groupid = sg['GroupId']
-                security_groupname = sg['GroupName']
+      for sg in instance.security_groups:
+        security_groupid = sg['GroupId']
+        security_groupname = sg['GroupName']
+        print(
+            "  + {}({}) with key:{} is using security groups: {}({})".format(
+                instance_name,
+                instance_state,
+                instance_key_name,
+                security_groupname,
+                security_groupid))
+        membership[instance_name] = security_groupid
+
+    for sg in boto3.client('ec2', region_name=region_name).describe_security_groups()[
+            'SecurityGroups']:
+      security_groupname = sg['GroupName']
+      security_groupid = sg['GroupId']
+
+      if security_groupid in membership.values():
+        print("  - {}({}): Used".format(security_groupname, security_groupid))
+      else:
+        if security_groupname == "default":
+          print(
+              "  - {}({}): skipped unused".format(security_groupname, security_groupid))
+        else:
+          print(
+              "  - {}({}): Unused".format(security_groupname, security_groupid))
+
+      for sg_item in sg['IpPermissions']:
+        if sg_item['IpProtocol'] == 'tcp':
+          sg_item_from_port = sg_item['FromPort']
+          sg_item_to_port = sg_item['ToPort']
+          sg_item_ip_range = sg_item['IpRanges']
+          if sg_item_from_port == 0 and sg_item_to_port > 0 and len(
+                  sg_item_ip_range):
+            for cidr_ip in sg_item_ip_range:
+              if '0.0.0.0/0' in cidr_ip.values():
+                hazard_sg[security_groupid] = security_groupname
                 print(
-                    "  + {}({}) with key:{} is using security groups: {}({})".format(
-                        instance_name,
-                        instance_state,
-                        instance_key_name,
-                        security_groupname,
-                        security_groupid))
-                membership[instance_name] = security_groupid
-
-        for sg in boto3.client('ec2', region_name=region_name).describe_security_groups()[
-                'SecurityGroups']:
-            security_groupname = sg['GroupName']
-            security_groupid = sg['GroupId']
-
-            if security_groupid in membership.values():
-                print("  - {}({}): Used".format(security_groupname, security_groupid))
-            else:
-                if security_groupname == "default":
-                    print(
-                        "  - {}({}): skipped unused".format(security_groupname, security_groupid))
-                else:
-                    print(
-                        "  - {}({}): Unused".format(security_groupname, security_groupid))
-
-            for sg_item in sg['IpPermissions']:
-                if sg_item['IpProtocol'] == 'tcp':
-                    sg_item_from_port = sg_item['FromPort']
-                    sg_item_to_port = sg_item['ToPort']
-                    sg_item_ip_range = sg_item['IpRanges']
-                    if sg_item_from_port == 0 and sg_item_to_port > 0 and len(
-                            sg_item_ip_range):
-                        for cidr_ip in sg_item_ip_range:
-                            if '0.0.0.0/0' in cidr_ip.values():
-                                hazard_sg[security_groupid] = security_groupname
-                                print(
-                                    "    (!!) WARNING Port TCP ({}-{}): {}".format(
-                                        sg_item_from_port, sg_item_to_port, sg_item_ip_range))
-                            else:
-                                print(
-                                    "    (*) Port TCP ({}-{}): {}".format(
-                                        sg_item_from_port,
-                                        sg_item_to_port,
-                                        sg_item_ip_range))
-                    else:
-                        print("    (*) Port TCP ({}-{}): {}".format(sg_item_from_port,
-                                                                    sg_item_to_port, sg_item_ip_range))
+                    "    (!!) WARNING Port TCP ({}-{}): {}".format(
+                        sg_item_from_port, sg_item_to_port, sg_item_ip_range))
+              else:
+                print(
+                    "    (*) Port TCP ({}-{}): {}".format(
+                        sg_item_from_port,
+                        sg_item_to_port,
+                        sg_item_ip_range))
+          else:
+            print("    (*) Port TCP ({}-{}): {}".format(sg_item_from_port,
+                                                        sg_item_to_port, sg_item_ip_range))
 
 
 if __name__ == '__main__':
-    scan_ec2_security_groups()
+  scan_ec2_security_groups()
